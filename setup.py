bac5637d48024717761247dc6b914cc76f7082cf
Vladimir Berkutov <vladimir.berkutov@gmail.com>
2020-04-10 21:53:47 +0100

    Add versioning

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pandas-f',
-    version='',
+    version='0.0.0',
     packages=find_packages(),
     url='http://github.com/dair-targ/pandas_f',
     license='Apache 2.0',
